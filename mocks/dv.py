282288c79770c321b79d582b43a90062e8ffd2db
Pete Meyer <pameyer@crystal.harvard.edu>
2018-07-05 16:49:37 -0400

    cleanup

--- mocks/dv.py
+++ mocks/dv.py
@@ -9,15 +9,10 @@ import json
 
 app = Flask( __name__ )
 
-#@app.route('/storage_query/<x>',methods=['GET'])
-#@app.route('/api/datasets/:persistentId/<x>',methods=['GET'])
-#def storage_id_query(x):
 @app.route('/api/datasets/:persistentId/',methods=['GET'])
 def storage_id_query():
     x = request.args.get('persistentId')
     r = {'x':x,'data':{'latestVersion':{'files':[{'dataFile':{'storageIdentifier':'16389c3b4de-8052ecdd77c1'}}]}}}
-    #return 'storage id would go here\n'
-    #return json.dumps(r)
     return Response( json.dumps(r), mimetype='application/json')
 
 @app.route('/resume/<x>', methods=['POST'])
