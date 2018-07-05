d574cd91d5f73f9a7ce9cf9f665995fb66e63778
Pete Meyer <pameyer@crystal.harvard.edu>
2018-07-05 16:49:15 -0400

    iterating on storage query mock

--- mocks/dv.py
+++ mocks/dv.py
@@ -4,13 +4,21 @@
 mock dataverse API endpoint for RSAL development
 '''
 
-from flask import Flask
+from flask import Flask,request,Response
+import json
 
 app = Flask( __name__ )
 
-@app.route('/storage_query/<x>',methods=['GET'])
-def storage_id_query(x):
-    return 'storage id would go here\n'
+#@app.route('/storage_query/<x>',methods=['GET'])
+#@app.route('/api/datasets/:persistentId/<x>',methods=['GET'])
+#def storage_id_query(x):
+@app.route('/api/datasets/:persistentId/',methods=['GET'])
+def storage_id_query():
+    x = request.args.get('persistentId')
+    r = {'x':x,'data':{'latestVersion':{'files':[{'dataFile':{'storageIdentifier':'16389c3b4de-8052ecdd77c1'}}]}}}
+    #return 'storage id would go here\n'
+    #return json.dumps(r)
+    return Response( json.dumps(r), mimetype='application/json')
 
 @app.route('/resume/<x>', methods=['POST'])
 def resume_workflow(x):
--- mocks/test_dv.py
+++ mocks/test_dv.py
@@ -12,4 +12,15 @@ class TestDV(TestCase):
     def test_index(self):
         r=self.app.get('/')
         self.assertEqual(200,r.status_code)
+    def test_storage_query(self):
+        dset='ThisIsNotARealPID'
+        r = self.app.get('api/datasets/:persistentId/', query_string={'persistentId':'doi:10.5072/FK2/%s'%dset})
+        self.assertEqual(200,r.status_code)
+        j = r.get_json()
+        fs = j['data']['latestVersion']['files']
+        self.assertEqual(1, len(fs))
+        pkg = fs[0]
+        sid = pkg['dataFile']['storageIdentifier']
+        self.assertEqual('16389c3b4de-8052ecdd77c1',sid) # aka - this is what we've got in testdata
+
 
